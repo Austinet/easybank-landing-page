249d770ff61c67bf7cfac87a63f89de3920cba6c
Austinet <austinetsplendour@gmail.com>
2023-10-07 20:34:51 +0100

    bg: fix gh-pages error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,35 +7,6 @@ import {BiLogoInstagram} from 'react-icons/bi'
 import {BiLogoPinterest} from 'react-icons/bi'
 import {BiLogoYoutube} from 'react-icons/bi'
 
-const socials = [
-  {
-    name: "facebook",
-    href: "/",
-    icon: <BiLogoFacebookSquare size='1.5rem' className="text-white text-xl hover:text-limeGreen"/>
-  },
-  {
-    name: "youtube",
-    href: "/",
-    icon: <BiLogoYoutube size='1.5rem' className="text-white text-xl hover:text-limeGreen"/>
-  },
-  {
-    name: "twitter",
-    href: "/",
-    icon: <BiLogoTwitter size='1.5rem' className="text-white text-xl hover:text-limeGreen"/>
-
-  },
-  {
-    name: "pinterest",
-    href: "/",
-    icon: <BiLogoPinterest size='1.5rem' className="text-white text-xl hover:text-limeGreen"/>
-  },
-  {
-    name: "instagram",
-    href: "/",
-    icon: <BiLogoInstagram size='1.5rem' className="text-white text-xl hover:text-limeGreen"/>
-  },
-];
-
 
 const Footer = () => {
   return (
@@ -44,16 +15,61 @@ const Footer = () => {
         <div className="lg:flex flex-col lg:gap-[3.5rem] lg:mx-0 mx-auto">
           <img src={logo} alt="Easybank logo" className="mx-auto lg:mx-0" />
           <ul className="flex items-center gap-5 mt-6 lg:mt-0">
-            {socials.map((link) => (
-              <li key={link.name}>
-                <Link
-                  to={link.href}
-                  className="text-[1.2rem] font-sans font-semibold hover:text-limeGreen"
-                >
-                  {link.icon}
-                </Link>
-              </li>
-            ))}
+            <li>
+              <Link
+                to={"/"}
+                className="text-[1.2rem] font-sans font-semibold"
+              >
+                <BiLogoFacebookSquare
+                  size="1.5rem"
+                  className="text-white text-xl hover:text-limeGreen"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={"/"}
+                className="text-[1.2rem] font-sans font-semibold"
+              >
+                <BiLogoYoutube
+                  size="1.5rem"
+                  className="text-white text-xl hover:text-limeGreen"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={"/"}
+                className="text-[1.2rem] font-sans font-semibold"
+              >
+                <BiLogoTwitter
+                  size="1.5rem"
+                  className="text-white text-xl hover:text-limeGreen"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={"/"}
+                className="text-[1.2rem] font-sans font-semibold"
+              >
+                <BiLogoPinterest
+                  size="1.5rem"
+                  className="text-white text-xl hover:text-limeGreen"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={"/"}
+                className="text-[1.2rem] font-sans font-semibold"
+              >
+                <BiLogoInstagram
+                  size="1.5rem"
+                  className="text-white text-xl hover:text-limeGreen"
+                />
+              </Link>
+            </li>
           </ul>
         </div>
         <div className="xl:w-[400px] lg:w-[300px] flex lg:justify-between lg:flex-row gap-5 flex-col text-center lg:text-left">
