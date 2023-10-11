e5ad2e43bc7a24f8f0acde408a7c5d9297f974b7
Austinet <austinetsplendour@gmail.com>
2023-10-12 00:28:49 +0100

    bg: fix base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/easybank-landing-page/",
+  base: "/easybank-landing-page/",
   plugins: [react()],
 })
